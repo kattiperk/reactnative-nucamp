02e96cdd556413ff04a5d77b82fecfe4543b5d53
Kateryna Masiuk <kmasiuk91@gmail.com>
2025-02-08 12:10:52 -0800

    week 4 task 1

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as ImagePicker from 'expo-image-picker';
 import { baseUrl } from '../shared/baseUrl';
 import logo from '../assets/images/logo.png';
+import ImageManipulator from 'expo-image-manipulator';
 
 const LoginTab = ({ navigation }) => {
     const [username, setUsername] = useState('');
@@ -148,9 +149,21 @@ const RegisterTab = () => {
             });
             if (capturedImage.assets) {
                 console.log(capturedImage.assets[0]);
-                setImageUrl(capturedImage.assets[0].uri);
+                // setImageUrl(capturedImage.assets[0].uri);
+                processImage(capturedImage.assets[0].uri);
             }
         }
+    };
+
+    const processImage = async (imageUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imageUri,
+            [{ resize: { width: 400 } }],
+            { format: 'png' }
+        );
+        console.log(processedImage);
+
+        setImageUrl(processedImage.uri);
     };
 
     return (
